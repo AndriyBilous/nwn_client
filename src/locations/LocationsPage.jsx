48f54efa7121e198523d7cd65989392a5fc779b1
AndriyBilous <andriy.bilous.92@gmail.com>
2023-06-29 13:38:56 +0300

    Changed some styles, so all pages render correctly

--- src/locations/LocationsPage.jsx
+++ src/locations/LocationsPage.jsx
@@ -2,5 +2,257 @@ import React from "react";
 import styles from "./LocationsPage.module.css";
 
 export const LocationsPage = () => {
-  return <div className={styles.locations__container}>LocationsPage</div>;
+  return (
+    <div className={styles.locations__container}>
+      <div className={styles.scheme__container}>
+        {/* First row */}
+        <div className={styles.size_2}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Чернолесье. Погост</p>
+          </div>
+        </div>
+        <div className={styles.size_2}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Чернолесье. Погост</p>
+          </div>
+        </div>
+        {/* Second row */}
+        <div className={styles.size_3}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Чернолесье</p>
+          </div>
+        </div>
+        <div className={styles.size_3}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Черная башня. Чаща</p>
+          </div>
+        </div>
+        <div className={styles.size_3}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Черная башня. Храм темных богов</p>
+          </div>
+        </div>
+        {/* Third row */}
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Иглы стужи. Подземный проход (застава)</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Иглы стужи. Плато</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Иглы стужи. Пещера</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Черная башня</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Карстовые пещеры. Подземное озеро</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Карстовые пещеры</p>
+          </div>
+        </div>
+        {/* Forth row */}
+        <div className={styles.size_5}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Иглы стужи. Предгорье. Ледяная тюрьма.</p>
+          </div>
+        </div>
+        <div className={styles.size_5}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Иглы стужи. Предгорье</p>
+          </div>
+        </div>
+        <div className={styles.size_5}>
+          <div className={styles.block__container}>
+            <p>
+              Безлюдные земли. Иглы стужи. Залив разбитых кораблей. (Побережье
+              моря бурь)
+            </p>
+          </div>
+        </div>
+        <div className={styles.size_5}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Великаньи пустоши. Руины в каньоне</p>
+          </div>
+        </div>
+        <div className={styles.size_5}>
+          <div className={styles.block__container}>
+            <p>
+              Безлюдные земли. Рачье герцогство. Инсанна. Лаборатория алхимика
+            </p>
+          </div>
+        </div>
+        {/* Fifth row */}
+        <div className={styles.size_4}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Ледяные поля</p>
+          </div>
+        </div>
+        <div className={styles.size_4}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Иглы стужи. Предгорье. Ледяная усыпальница</p>
+          </div>
+        </div>
+        <div className={styles.size_4}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Великаньи пустоши. Заброшенная застава</p>
+          </div>
+        </div>
+        <div className={styles.size_4}>
+          <div className={styles.block__container}>
+            <ol>
+              <span>Корабли в:</span>
+              <li>Побережье моря бурь</li>
+              <li>Заграба</li>
+              <li>Стальная пристань</li>
+              <li>Гаррак</li>
+              <li>Хребет мира</li>
+            </ol>
+          </div>
+        </div>
+        {/* Sixth row */}
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Д'Сан'Дор. Ельник (застава)</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Д'Сан'Дор</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Великаньи пустоши</p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>
+              Безлюдные земли. Рачье герцогство. Инсанна. Заброшенные катакомбы
+            </p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>
+              Безлюдные земли. Рачье герцогство. Инсанна. Канализация. Стоки
+            </p>
+          </div>
+        </div>
+        <div className={styles.size_6}>
+          <div className={styles.block__container}>
+            <p>
+              Безлюдные земли. Рачье герцогство. Инсанна. Канализация. Отстойник
+            </p>
+          </div>
+        </div>
+        {/* Sevent`s row */}
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Деревня в снегах</p>
+          </div>
+        </div>
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <ol>
+              <li>Грифон в южный форт</li>
+            </ol>
+          </div>
+        </div>
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Великаньи пустоши. Заводь</p>
+          </div>
+        </div>
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Иглы стужи. Пещера великанов</p>
+          </div>
+        </div>
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Рачье герцогство. Развалины первой крепости</p>
+          </div>
+        </div>
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Рачье герцогство. Инсанна. Ворота</p>
+          </div>
+        </div>
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Рачье герцогство. Инсанна.</p>
+          </div>
+        </div>
+        <div className={styles.size_8}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Рачье герцогство. Инсанна. Канализация</p>
+          </div>
+        </div>
+        {/* Eighth row */}
+        <div className={styles.size_7}>
+          <div className={styles.block__container}>
+            <p>
+              Безлюдные земли. Дорога на Д'Сан'Дор. Застава Рачьего Герцогства.
+            </p>
+          </div>
+        </div>
+        <div className={styles.size_7}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Дорога на Д'Сан'Дор.</p>
+          </div>
+        </div>
+        <div className={styles.size_7}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Великаньи пустоши. Дикое ущелье</p>
+          </div>
+        </div>
+        <div className={styles.size_7}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Старая дорога через лес.</p>
+          </div>
+        </div>
+        <div className={styles.size_7}>
+          <div className={styles.block__container}>
+            <p>
+              Безлюдные земли. Рачье Герцогство. Драконий замок. Предместная
+              крепость
+            </p>
+          </div>
+        </div>
+        <div className={styles.size_7}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Рачье Герцогство. Инсанна. Драконий замок</p>
+          </div>
+        </div>
+        <div className={styles.size_7}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Рачье Герцогство. Инсанна. Арена</p>
+          </div>
+        </div>
+        {/* Ninth row */}
+        <div className={styles.size_2}>
+          <div className={styles.block__container}>
+            <p>Безлюдные земли. Рачье Герцогство. Инсанна. Арена</p>
+          </div>
+        </div>
+        <div className={styles.size_7}>
+          <div className={styles.block__container + " " + styles.hidden}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
